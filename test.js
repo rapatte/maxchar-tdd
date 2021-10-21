b16f19fbf7cc4fa9cc4ded93cec5c35750371213
antoine <antoine.gayoso@gmail.com>
2021-10-21 11:47:58 +0200

    3 tests

---
+++ test.js
@@ -0,0 +1,14 @@
+const maxChar = require("index")
+
+test("function exists", () => {
+    expect(typeof maxChar).toEqual("function");
+})
+
+test("finds the most frequently used char", () => {
+    expect(maxChar("a")).toEqual("a");
+    expect(maxChar("azlkhdslkfhdsqfaaaaaaaa")).toEqual("a")
+})
+
+test("works with number in the string", () => {
+    expect(maxChar("a1b1c1d1e1")).toEqual("1");
+})
